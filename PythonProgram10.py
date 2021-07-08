5d8dffb09d8006ae92ca442c1fa198cc4ddb1e02
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-08 21:51:59 +0600

    Create PythonProgram10.py

---
+++ PythonProgram10.py
@@ -0,0 +1,6 @@
+# Parallelogram ; Area = b * h ; [ b = base , h = vertical height ]
+
+base = int(input("Enter base : "))
+height = int(input("Enter height : "))
+area = base * height
+print("area is = ",area)
